65a1c57846065e627202f4e665a4e7a542fb7a71
Administrator <abdusshobhan.4444@gmail.com>
2024-11-16 16:23:46 +0600

    add Banner

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../Component/Nav';
 import Footer from '../Component/Footer';
+import Banner from '../Component/Home/Banner';
 
 const Home = () => {
     return (
         <div>
             <Nav></Nav>
+            <Banner></Banner>
             <div className='min-h-screen'>
-
+     
             </div>
 
             <div>
