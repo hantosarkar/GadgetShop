15275dd1a106d100a303206bd591706150f83a12
Administrator <abdusshobhan.4444@gmail.com>
2024-11-16 16:13:54 +0600

    add Footer

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../Component/Nav';
+import Footer from '../Component/Footer';
 
 const Home = () => {
     return (
         <div>
-              <Nav></Nav>
+            <Nav></Nav>
+            <div className='min-h-screen'>
+
+            </div>
+
+            <div>
+                 <Footer></Footer>
+            </div>
         </div>
     );
 };
