9c8e0ac739b346cab82ed8acc85eb45fe6f56a74
Administrator <abdusshobhan.4444@gmail.com>
2024-11-21 11:09:39 +0600

    add Search Product successfully

--- src/Page/Product.jsx
+++ src/Page/Product.jsx
@@ -4,8 +4,9 @@ import Footer from '../Component/Footer';
 import axios from 'axios';
 import Products from '../Component/Product/Products';
 import { GrPowerReset } from "react-icons/gr";
+import { IoIosSearch } from "react-icons/io";
 const Product = () => {
-
+    const [loading, setLoading] = useState(false);
     const [products, setProduct] = useState([]);
     const [Category, setCategory] = useState([]);
     const [brand, setBrand] = useState([]);
@@ -13,22 +14,25 @@ const Product = () => {
     const [sort, setSort] = useState('ASC');
     const [CategorySearch, setCategorySearch] = useState('');
     const [brandSearch, setBrandSearch] = useState('');
-
+    const [search, setSearch] = useState('');
 
 
     useEffect(() => {
+        setLoading(true);
         const fetch = () => {
-            axios.get(`http://localhost:3000/Product?Brand=${brandSearch}&Category=${CategorySearch}&Short=${sort}`)
+            axios.get(`http://localhost:3000/Product?Brand=${brandSearch}&Category=${CategorySearch}&Short=${sort}&search=${search}`)
                 .then(res => {
                     if (res.data) {
                         setProduct(res.data.Product);
                         setCategory(res.data.allCategory);
                         setBrand(res.data.allBrand);
+                        setLoading(false);
                     }
+
                 })
         }
         fetch();
-    }, [sort, CategorySearch, brandSearch])
+    }, [sort, CategorySearch, brandSearch, search])
 
     const handleShort = (e) => {
         setSort(e.target.value);
@@ -38,12 +42,23 @@ const Product = () => {
 
     const handleBrandSearch = (e) => {
         setCategorySearch("");
+        setSearch("")
         setBrandSearch(e.target.value)
     }
 
     const handleCategorySearch = (e) => {
+        setSearch("")
         setBrandSearch("");
         setCategorySearch(e.target.value)
+
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        setCategorySearch("");
+        setBrandSearch("");
+        setSearch(e.target.search.value);
 
     }
 
@@ -60,53 +75,67 @@ const Product = () => {
         <div>
             <Nav></Nav>
             <div className='min-h-screen container mx-auto'>
-                <div class="flex gap-4 justify-between p-6 mt-10">
-                    <div class="bg-base-200 w-52 min-h-screen p-3 rounded-t-md">
-                        <h1 className='text-center p-2 text-xl font-bold'>Filter</h1>
-                        <div className='space-y-3'>
-                            <div>
-                                <h1 className='text-xs font-thin mb-1'>Category</h1>
-                                <select onClick={handleCategorySearch} className="select select-bordered w-full max-w-xs">
+                <div class="flex flex-col-reverse md:flex-row lg:flex-row gap-5 justify-between p-6 mt-10">
+                    <div class="w-full md:w-60 lg:w-60 p-3 rounded-t-md">
+                        <div className='pb-2'>
+                            <form onSubmit={handleSearch}>
+                                <div className='flex gap-1'>
+                                    <input type="text" name='search' placeholder='Search' className='border outline-none p-1 px-2 rounded-l-md' />
+                                    <button className='border rounded-r-md p-1 px-2 hover:bg-slate-400 w-full '><IoIosSearch /></button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="bg-base-200 w-full md:w-60 lg:w-60  p-3 rounded-t-md">
+                            <h1 className='text-center p-2 text-xl font-bold'>Filter</h1>
+                            <div className='space-y-3'>
+                                <div>
+                                    <h1 className='text-xs font-thin mb-1'>Category</h1>
+                                    <select onClick={handleCategorySearch} className="select select-bordered w-full max-w-xs">
 
-                                    {
+                                        {
 
-                                        Category.map((cate, i) =>
-                                            <option key={i}>{cate}</option>
-                                        )
-                                    }
+                                            Category.map((cate, i) =>
+                                                <option key={i}>{cate}</option>
+                                            )
+                                        }
 
-                                </select>
-                            </div>
-                            <div>
-                                <h1 className='text-xs font-thin  mb-1'>Brand</h1>
-                                <select onClick={handleBrandSearch} className="select select-bordered w-full max-w-xs">
-                                    {
+                                    </select>
+                                </div>
+                                <div>
+                                    <h1 className='text-xs font-thin  mb-1'>Brand</h1>
+                                    <select onClick={handleBrandSearch} className="select select-bordered w-full max-w-xs">
+                                        {
 
-                                        brand.map((brand, i) =>
-                                            <option key={i} value={brand}>{brand}</option>
-                                        )
-                                    }
-                                </select>
-                            </div>
-                            <div>
-                                <button  onClick={handleReset} className='btn w-full bg-gray-400'>
-                                  <GrPowerReset/>  Reset
+                                            brand.map((brand, i) =>
+                                                <option key={i} value={brand}>{brand}</option>
+                                            )
+                                        }
+                                    </select>
+                                </div>
+                                <div>
+                                    <button onClick={handleReset} className='btn w-full bg-gray-400'>
+                                        <GrPowerReset />  Reset
                                     </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="flex-1">
 
-                        <div className='grid gap-2 grid-cols-3'>
+                        {loading && <div className=' text-center p-10'><span className="loading loading-bars loading-sm"></span></div>}
 
+                        {
+                            products.length < 1 && <div className='w-full flex justify-center items-center min-h-screen text-center'>
+                                <img src="./../../public/Images/noDataFound.png" alt="" />
+                            </div>
+                        }
+                        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2 '>
                             {
                                 products.map((products, i) =>
                                     <Products key={i} products={products} ></Products>
                                 )
                             }
                         </div>
-
-
                     </div>
                     <div class="w-52">
                         <div className='flex gap-3 items-center'>
